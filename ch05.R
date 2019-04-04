7db09f818d71467fc04584222ee655527a2fd6e4
HaeJi-Woo <whj1939@cu.ac.kr>
2019-04-04 11:55:26 +0900

    sff

--- ch05.R
+++ ch05.R
@@ -9,3 +9,14 @@ summary(midwest)
 library(dplyr)
 midwset <- rename(midwest, total = poptotal)
 midwest <- rename(midwest, asian = popasian)
+
+midwest$ratio <- midwest$asian/midwest$total*100
+hist(midwest$ratio)
+
+mean(midwest$ratio)
+
+midwest$group <- ifelse(midwest$ratio > 0.4872462, "large", "small")
+
+tabel(midwest$group)
+library(ggplot2)
+qplot(midwest$group)
