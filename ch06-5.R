fa92077338c5e17cf6360cad81ef7687e05c20ae
HaeJi-Woo <whj1939@cu.ac.kr>
2019-04-16 14:40:28 +0900

    add

--- ch06-5.R
+++ ch06-5.R
@@ -25,23 +25,35 @@ mpg %>%
   arrange(desc(mean_tot)) %>%
   head(5)
 
-mpg <- as.data.as(ggplot2)
-mpg %>%
-  group_by(class) %>%
-  summarise(mean_cty = mean(cty)) 
+test1 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(60, 80, 70, 90, 85))
+test2 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    final = c(70, 83, 65, 95, 80))
+test1
+test2
+total <- left_join(test1, test2, by = "id")
+total
+name <- data.frame(class = c(1, 2, 3, 4, 5),
+                   teacher = c("kim", "lee", "park", "choi", "jung"))
+name
+exam_new <- left_join(exam, name, by = "class")
+exam_new
+group_a <- data.frame(id = c(1, 2, 3, 4, 5),
+                      test = c(60, 80, 70, 90, 85))
+group_b <- data.frame(id = c(6, 7, 8, 9, 10),
+                      test = c(70, 83, 65, 95, 80))
+group_a
+group_b
 
-mpg %>% group_by(class) %>%
-  summarise(mean_cty = mean(cty)) %>%
-  arrange(desc(mean_cty))
+fuel <- data.frame(fl = c("c", "d", "e", "p", "r"),
+                   price_fl = c(2.35, 2.38, 2.11, 2.76, 2.22), 
+                   stringsAsFactors = F) 
+fuel
 
-mpg %>%
-  group_by(manufacturer) %>%
-  summarise(mean_hwy = mean(hwy)) %>%
-  arrange(desc(mean_hwy)) %>%
-  head(3)
+mpg <- as.data.frame(ggplot2::mpg)
+mpg <- left_join(mpg, fuel, by = "fl")
 
-mpg %>% filter(class == "compact") %>%
-  group_by(manufacturer) %>%
-  summarise(count = n()) %>%
-  arrange(desc(count))
+mpg %>% 
+  select(model, fl, price_fl) %>%
+  head(5)
   
